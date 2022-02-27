6e9d943dbcfa13b85f7dcb20398b6f9b8a9750b2
ryanreszetnik <rres@rogers.com>
2022-02-27 01:23:40 -0500

    graphs

--- FRONTEND/src/App.tsx
+++ FRONTEND/src/App.tsx
@@ -4,11 +4,10 @@ import Tabs from "./Components/Tabs";
 // Tabs Components
 import TabOne from "./Components/TabOne";
 import TabTwo from "./Components/TabTwo";
-import TabThree from "./Components/TabThree";
 import axios from "axios";
 import { backendUrl } from "./Constants";
 import { getSentiment } from "./Endpoints";
-
+import FacebookLogin from "react-facebook-login";
 type TabsType = {
   label: string;
   index: number;
@@ -27,33 +26,14 @@ const tabs: TabsType = [
     index: 2,
     Component: TabTwo,
   },
-  {
-    label: "Twitter",
-    index: 3,
-    Component: TabThree,
-  },
 ];
 
 export default function App() {
   const [selectedTab, setSelectedTab] = useState<number>(tabs[0].index);
-  const [input, setInput] = useState("");
-  const [returned, setReturned] = useState("");
-  const submit = async () => {
-    const ret = await getSentiment(input);
-    console.log(ret);
-    setReturned(ret);
-    setInput("");
-  };
+
   return (
     <div className="App">
       <h1>See your social statistics!</h1>
-      <input
-        type="text"
-        onChange={(e) => setInput(e.target.value)}
-        value={input}
-      ></input>
-      <button onClick={submit}>Send</button>
-      <h1>{JSON.stringify(returned)}</h1>
       <h2>Select a platform to view information:</h2>
       <br />
       <Tabs selectedTab={selectedTab} onClick={setSelectedTab} tabs={tabs} />
