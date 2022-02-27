b1faba6d22d745241b0b5166be72534063741492
sabrina0822 <90227881+sabrina0822@users.noreply.github.com>
2022-02-27 04:57:06 -0500

    chanfes

--- FRONTEND/src/Components/TabOne.tsx
+++ FRONTEND/src/Components/TabOne.tsx
@@ -1,6 +1,24 @@
-import React, { FC, Fragment } from "react";
+import React, { FC, Fragment, useEffect, useState } from "react";
+import { getBasicInsta } from "../Endpoints";
 
 const TabOne: FC<{}> = () => {
+    const [username, setUsername] = useState("");
+    const [info, setInfo] = useState({});
+    // const [events, setEvents] = useState(data);
+    // const [posts, setPosts] = useState([{ time: 0, display_time: 0 }]);
+    const loadBasics = async () => {
+      const  {basics, dailyData} = await getBasicInsta()
+      console.log(basics,dailyData)
+    };
+    const loadPosts = async () => {
+    //   setPosts(await getPosts());
+    };
+    useEffect(() => {
+      loadBasics();
+    //   loadEvents();
+    //   loadPosts();
+    }, []);
+  
   return (
     <Fragment>
       <h3>Instagram</h3>
