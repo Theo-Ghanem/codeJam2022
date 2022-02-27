288c3b22dcaf8a5967ced024fdf3e94447203ebe
Theo-Ghanem <90062145+Theo-Ghanem@users.noreply.github.com>
2022-02-27 02:14:51 -0500

    ui design + added 2 tabs

---
+++ FRONTEND/src/Components/TabFour.tsx
@@ -0,0 +1,13 @@
+import React, { FC, Fragment } from "react";
+
+const TabFour: FC<{}> = () => {
+  return (
+    <Fragment>
+      <h3>Login</h3>
+      <p>
+        You are already logged in !
+      </p>
+    </Fragment>
+  );
+};
+export default TabFour;
